37b9051d9aca8aa01f395add98bd05d240529d2e
arafat-zip <powergenyt@gmail.com>
2024-10-20 23:36:15 +0600

    taxonomy added successfully

--- movie-db/includes/registing-post-type.php
+++ movie-db/includes/registing-post-type.php
@@ -30,21 +30,40 @@ class MovieDb_Post_Type {
 
     public function register_taxonomy() {
         $args = [
-            'label' => 'Genres',
+            'label' => 'Genre',
             'public' => true,
             'labels' => [
-                'name' => 'Genres',
+                'name'          => 'Genre',
                 'singular_name' => 'Genre',
-                'add_new_item' => 'Add New Genre',
-                'edit_item' => 'Edit Genre',
-                'new_item' => 'New Genre',
-                'view_item' => 'View Genre',
-                'search_items' => 'Search Genres',
-                'not_found' => 'No Genres Found',
-                'not_found_in_trash' => 'No Genres Found in Trash',
-                'menu_name' => 'Genres',
-            ]
+                'menu_name'          => 'Genre',
+            ],
+            'hierarchical'           => false
         ];
         register_taxonomy( 'genre', 'movie', $args );
+
+
+        $args = [
+            'label'  => 'actor',
+            'public' => true,
+            'labels' => [
+                'name'          => 'Actors',
+                'singular_name' => 'actor',
+                'menu_name'     => 'Genre',
+            ],
+            'hierarchical'      => false
+        ];
+        register_taxonomy( 'actor', 'movie', $args );
+
+        $args = [
+            'label'  => 'Director',
+            'public' => true,
+            'labels' => [
+                'name'          => 'Directors',
+                'singular_name' => 'director',
+                'menu_name'     => 'director',
+            ],
+            'hierarchical'           => false
+        ];
+        register_taxonomy( 'director', 'movie', $args );
     }
 }
